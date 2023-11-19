02fe7fe7c1bb695aa7f9eab9b4715e4c4b6b3fe3
RacquelNinaDennison <racquelnina@gmail.com>
2023-11-19 18:46:19 +0200

    completed app

--- multiplication/src/app/page.tsx
+++ multiplication/src/app/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 import { useState } from "react";
 import styles from "../../styles/timestables.module.scss";
-
+import toast, { Toaster } from "react-hot-toast";
 export default function Practice() {
 	const [numberChoosen, setNumberChoosen] = useState(0);
 	const [numberPicked, setNumberPicked] = useState(false);
@@ -25,6 +25,11 @@ export default function Practice() {
 		const correctAnswer = numberChoosen * currentQuestion;
 		if (parseInt(userAnswer) === correctAnswer) {
 			setScore(score + 1);
+			toast.success("Well done Grayson!");
+		} else {
+			toast.error(
+				`Almost there, the answer is ${correctAnswer}`
+			);
 		}
 
 		setCurrentQuestion(randomInt(1, 12));
@@ -33,8 +38,29 @@ export default function Practice() {
 
 	return numberPicked ? (
 		<main>
+			<Toaster
+				toastOptions={{
+					className: "",
+					style: {
+						border: "1px solid #000000",
+						padding: "16px",
+						color: "#000000",
+					},
+				}}
+			/>
 			<div className={styles.body}>
-				<div className={styles.container}>
+				<div className={styles.pContainer}>
+					<p className={styles.paragraph}>
+						Enter the correct answer
+					</p>
+					<p className={styles.paragraph}>
+						You've got this Grayson!
+					</p>
+				</div>
+
+				<div
+					className={`${styles.container} ${styles.questionContainer}`}
+				>
 					<div className={styles.question}>
 						<div className={styles.questionsContainer}>
 							<div className={styles.questionButton}>
@@ -86,15 +112,26 @@ export default function Practice() {
 							</button>
 						</div>
 					</div>
-					<p>Score: {score}</p>
 				</div>
-
-				<div className={styles.grass}></div>
+				<div className={styles.soccerBallContainer}>
+					<div className={styles.soccerBall}></div>
+				</div>
+				<div className={styles.numberContainer}>
+					<div className={styles.number}></div>
+					<div className={styles.two}></div>
+					<div className={styles.three}></div>
+				</div>
 			</div>
 		</main>
 	) : (
 		<main>
+			<Toaster />
 			<div className={styles.body}>
+				<div className={styles.pContainer}>
+					<p className={styles.paragraph}>
+						Choose which timetables you want to practice!
+					</p>
+				</div>
 				<div className={styles.container}>
 					<div className={styles.box}>
 						{[...Array(12)].map((_, index) => (
@@ -110,7 +147,6 @@ export default function Practice() {
 						))}
 					</div>
 				</div>
-				<div className={styles.grass}></div>
 			</div>
 		</main>
 	);
